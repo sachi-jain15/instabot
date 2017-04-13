b891f4c4dbcb365b962828f3f145858f34731823
unknown <sachi-jain15>
2017-04-13 16:53:04 +0530

    Search a word in comment

--- instabot.py
+++ instabot.py
@@ -6,7 +6,7 @@ APP_ACCESS_TOKEN = '1598029108.a88c6e0.b2a4559815d640e7be0299013d01a1a8'
 # Base URL common for all the requests in the file.
 BASE_URL = 'https://api.instagram.com/v1/'
 
-
+# Function below is to check whether we have succeeded or not.
 def success_or_failure(data):
     print "\n************************************************************************************************"
     if data['meta']['code'] == 200:
@@ -32,7 +32,7 @@ def owner_info():
         print "Website                  :", info_of_owner["data"]["website"]
     if info_of_owner['data']['bio'] != '':  # If Bio of the owner is mentioned
         print "Bio                      :", info_of_owner["data"]["bio"]
-    success_or_failure(info_of_owner)
+    success_or_failure(info_of_owner) # To check whether we have succeeded or not.
 
 
 owner_info()
@@ -110,8 +110,8 @@ def like_user_post(username, option, selection):
     post_id = get_post_by_choice(username, option, selection)  # get_user_post_id()function called here to get post ID
     payload = {'access_token': APP_ACCESS_TOKEN}
     like_post_url = BASE_URL + "media/" + post_id + "/likes"
-    like = requests.post(like_post_url, payload).json()
-    success_or_failure(like)
+    like = requests.post(like_post_url, payload).json()  # Post call to like the post
+    success_or_failure(like) # To check whether we have succeeded or not.
 
 
 like_user_post("api_17790", 0, 1)
@@ -124,7 +124,40 @@ def post_comment(username, option, selection):
     url_post_comment = BASE_URL + "media/" + media_id + "/comments"
     input_comment = raw_input("Write a comment you want to post.\n")
     request_data = {"access_token": APP_ACCESS_TOKEN, 'text': input_comment}  # Required to created a comment.
-    comment = requests.post(url_post_comment, request_data).json()
-    success_or_failure(comment)
+    comment = requests.post(url_post_comment, request_data).json()  # Post call to comment the post
+    success_or_failure(comment) # To check whether we have succeeded or not.
+
 
 post_comment("api_17790", 0, 1)
+
+
+# The Function returns comment Id that contains a particular word in a particular post
+def word_search_in_comment(username, option, selection):
+    media_id = get_post_by_choice(username, option, selection)  # get_user_post_id(username) function called here to get post ID
+    url_post_comment = BASE_URL + "media/" + media_id + "/comments?access_token=" + APP_ACCESS_TOKEN
+    various_comments = requests.get(url_post_comment).json()
+    word_search = raw_input("Enter a word you want to search in the comments")
+    print "\n************************************************************************************************"
+    comments_id = []
+    comments_list = []
+    user_name = []
+    for each_comment in various_comments['data']:
+        comments_list.append(each_comment['text'])
+        comments_id.append(each_comment['id'])
+        user_name.append(each_comment['from']['username'])
+    comments_id_matched = []
+    comments_matched = []
+    user_found = []
+    for each_item in range(len(comments_list)):
+        if word_search in comments_list[each_item]:
+            comments_matched.append(comments_list[each_item])
+            comments_id_matched.append(comments_id[each_item])
+            user_found.append(user_name[each_item])
+    if len(comments_matched) == 0:
+        print "No comment have " + word_search + " word"
+        return False,media_id, False, False
+    else:
+        print "Following are the comments that contains the " + word_search + " word:"
+        for i in range(len(comments_matched)):
+            print("-->  " + comments_matched[i])
+        return comments_id_matched, media_id, comments_matched, user_found
